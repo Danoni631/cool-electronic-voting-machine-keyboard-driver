2b983133362ce5a4cb938fd0c9b05b3acc8ec8b1
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-14 18:59:23 -0300

    Add keyboard header file with key definitions
    Define keyboard handling functions and key mappings.

---
+++ kb.h
@@ -0,0 +1,29 @@
+#ifndef KB_H
+#define KB_H
+
+#include "std.h"
+
+typedef enum
+{
+    KEY_A = 0x80,
+    KEY_B = 0x81,
+    KEY_C = 0x82
+} Keys;
+
+#define INVALID_KEY 0xFF
+
+// EVM = Eletronic Voting Machine
+typedef VOID (*EVMCallback)(BYTE key);
+
+static const BYTE key_map[4][4] =
+{
+    { '1', '2', '3', KEY_A   },
+    { '4', '5', '6', KEY_B  },
+    { '7', '8', '9', KEY_C },
+    { INVALID_KEY, '0', INVALID_KEY, INVALID_KEY }
+};
+
+VOID init_keyboard(EVMCallback callback);
+void tract_key(BYTE line_mask, BYTE column_mask);
+
+#endif
