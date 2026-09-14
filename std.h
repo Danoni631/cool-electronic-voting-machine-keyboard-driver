c557a77de6d193439d54a8a3131d4c5c09f1e7d3
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-14 18:40:27 -0300

    Add typedefs for common data types in std.h

---
+++ std.h
@@ -0,0 +1,8 @@
+// Unnecessary if you have
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned long ULONG;
+
+typedef char CHAR;
+#define VOID void
