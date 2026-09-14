1d1b2fbb06db829a484c93e1c42ca1d3e5ec2d7a
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-14 18:42:14 -0300

    Add assembly functions for inb and outb operations

---
+++ ports.asm
@@ -0,0 +1,14 @@
+// only if you want to use ports in/out made in asm
+[global inb]
+[global outb]
+
+inb:
+    mov dx, [esp + 4]
+    in al, dx
+    ret
+
+outb:
+    mov dx, [esp + 4]
+    mov al, [esp + 8]
+    out dx, al
+    ret
